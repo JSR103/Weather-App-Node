dc0cfc834c2bcab9f0f0bc8bf564520249a82c64
JSR103 <silva.joel.r@gmail.com>
2017-06-29 10:20:48 -0700

    I updated: now I am chaining callbacks

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-//   if(errorMessage){
-//     console.log(errorMessage);
-//   }else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-const request = require('request');
+const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
-request({
-  url: 'https://api.darksky.net/forecast/8203c75a0565e4f5fd1e4b1e816b4292/44.059912,-123.0179348',
-  json: true
-},(error,response,body) =>{
-  if(!error && response.statusCode === 200){
-    console.log(body.currently.temperature);
-}else {
-  console.log('Unable to fetch weather.');
-}
-  console.log(body.currently.temperature);
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+  if(errorMessage){
+    console.log(errorMessage);
+  }else {
+    console.log(results.address);
+    weather.getWeather(results.latitude,results.longitude, (errorMessage, weatherResults) =>{
+      if(errorMessage){
+          console.log(errorMessage);
+      }else{
+        console.log(`Its currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
+  }
 });
